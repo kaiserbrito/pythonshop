78d815283706e913aa0b03b290f725ec3ef9cd4e
kaiserbrito <victorbcguerra@gmail.com>
2017-07-06 11:24:12 -0300

    created the Catalog app, added base.html, home.html and edited the views, models and urls

--- pythonShop/settings.py
+++ pythonShop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
