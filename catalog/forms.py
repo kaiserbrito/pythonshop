99fb35a4bdc544955964bce420465fc6df658843
kaiserbrito <victorbcguerra@gmail.com>
2017-07-07 15:44:03 -0300

    implementing the user signup

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,10 +4,9 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, help_text='Required.')
+    first_name = forms.CharField(max_length=30, help_text='Required.'),
     last_name = forms.CharField(max_length=30, help_text='Required'),
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid e-mail address.')
-
 
     class Meta:
         model = User
