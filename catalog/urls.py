6efb9a906bec8d1215211bde52ed11f6d8a586f1
kaiserbrito <victorbcguerra@gmail.com>
2017-07-07 10:47:26 -0300

    starting to implement User registration

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Home.as_view()),
+    url(r'^signup/$', views.signup, name='signup'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.views.generic import View
 
 # Create your views here.
 
+
 class Home(View):
     def get(self, request, *args):
         return render(request, 'home.html')
+
+
+def signup(request):
+
+
